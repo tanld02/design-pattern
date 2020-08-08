142fe430d825f22a51ee82efcc728bc071f9a4be
OS <OS@DESKTOP-TENFEDN>
2020-08-08 11:41:36 +0700

    initial structural patterns

---
+++ KV.E.DesignPattern.Structural/KV.E.DesignPattern.Structural.Composite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KV.E.DesignPattern.Structural.Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ KV.E.DesignPattern.Structural/KV.E.DesignPattern.Structural.Facade/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KV.E.DesignPattern.Structural.Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
