bcc47bc534974c8cdd06400c373cba012cd18ddc
OS <OS@DESKTOP-TENFEDN>
2020-08-11 22:58:54 +0700

    feat: add builder

---
+++ KV.E.DesignPattern.Creational/KV.E.DesignPattern.Creational.Builder/Implement/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KV.E.DesignPattern.Creational.Builder.Implement
+{
+    public class Product
+    {
+        private List<object> _part = new List<object>();
+        public void Add(string part)
+        {
+            this._part.Add(part);
+        }
+        public string ListParts()
+        {
+            string str = string.Empty;
+            for(int i = 0;i < this._part.Count; i++)
+            {
+                str += this._part[i] + ", ";
+            }
+            str = str.Remove(str.Length - 2);
+
+            return $"Product parts: {str} \n";
+        }
+    }
+}
---
+++ KV.E.DesignPattern.Creational/KV.E.DesignPattern.Creational.Builder/Interface/IBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KV.E.DesignPattern.Creational.Builder.Interface
+{
+    public interface IBuilder
+    {
+        void BuildPartA();
+        void BuildPartB();
+        void BuildPartC();
+    }
+}
