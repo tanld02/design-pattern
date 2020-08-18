af5511205fa29951b12c9d6df210a9c4143fcf08
OS <OS@DESKTOP-TENFEDN>
2020-08-18 09:12:41 +0700

    feat: add singleton pattern

---
+++ KV.E.DesignPattern.Creational/KV.E.DesignPattern.Creational.Singleton/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KV.E.DesignPattern.Creational.Singleton
+{
+    public class Singleton
+    {
+        private Singleton() {}
+        private static Singleton _instance;
+        public static Singleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
